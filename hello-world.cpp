b12552565e85f349c30fdf9a6bf22e7e517398d5
famoses <andreas.osterberg@online.de>
2016-05-01 11:21:46 +0200

    yet another change on the main branch

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,8 +1,12 @@
 // hello-world.cpp : Defines the entry point for the console application.
 //
 
+// -------- include ------------ 
+
 #include "stdafx.h"
 #include <iostream> 
+
+// -------- functions ----------
 
 int main(int argc, char* argv[])
 {
