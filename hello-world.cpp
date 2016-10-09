e7093c6ed235a8a9c0f64030e8c43b1c1820bfc7
famoses <andreas.osterberg@online.de>
2016-10-09 19:45:56 +0200

    br_abzweig 1 Änderung der Kommentierung und neues heade file für strings

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,9 +1,14 @@
 // hello-world.cpp : Defines the entry point for the console application.
 //
 
+/* -----------------------*/
 #include "stdafx.h"
 #include <iostream> 
+#include <string>
+/* -----------------------*/
 
+
+// main entry point
 int main(int argc, char* argv[])
 {
 	std::cout<<"Hello World!\n";
